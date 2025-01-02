97e66c0eb97f7c2fc3e25ec9f41c20493f542004
Rafael P Lourenço <rafael.lourenco@consilux.com.br>
2025-01-02 16:38:23 -0300

    add card components and styles for card display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardsModule } from './cards/cards.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    CardsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/card/card.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+export class CardComponent {
+  plano = 
+  {
+    infos:
+    {
+      tipo: 'Simples',
+      valor: 100, 
+    }
+  }
+}
