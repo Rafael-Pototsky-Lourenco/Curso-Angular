ba0e1bcff6d61d48634e1532b3d14a6bdfc7c9e3
Rafael P Lourenço <rafael.lourenco@consilux.com.br>
2025-01-13 16:02:24 -0300

    Added buttons CSS Class and Style Bindings.

--- Seção 4 - Fundamentos do Angular - Binding Diretivas, Templates e Decorators/Aula 49 á 71 - Binding, Diretivas, Templates e Decorators/src/app/app.component.ts
+++ Seção 4 - Fundamentos do Angular - Binding Diretivas, Templates e Decorators/Aula 49 á 71 - Binding, Diretivas, Templates e Decorators/src/app/app.component.ts
@@ -6,54 +6,88 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'AULA_BINDING_TEMPLATES_DECORATORS';
-  inputText = 'Texto inicial Alterado!';
-  inputType = 'text';
-  isDisabled = true;
+  // title = 'AULA_BINDING_TEMPLATES_DECORATORS';
+  // inputText = 'Texto inicial Alterado!';
+  // inputType = 'text';
+  // isDisabled = true;
 
-  enableInput()
-  {
-    this.isDisabled = false;
-  }
+  // enableInput()
+  // {
+  //   this.isDisabled = false;
+  // }
 
-  disableInput()
-  {
-    this.isDisabled = true;
-  }
+  // disableInput()
+  // {
+  //   this.isDisabled = true;
+  // }
 
-  setPasswordTypeInput()
-  {
-    this.inputType = 'password';
-  }
+  // setPasswordTypeInput()
+  // {
+  //   this.inputType = 'password';
+  // }
 
-  setTextTypeInput()
-  {
-    this.inputType = 'text';
-  }
+  // setTextTypeInput()
+  // {
+  //   this.inputType = 'text';
+  // }
 
-  logInputText()
-  {
-    console.log(this.inputText);
-  }
+  // logInputText()
+  // {
+  //   console.log(this.inputText);
+  // }
 
-  handleInputKeyup(event: KeyboardEvent)
-  {
-    const currentText = (event.target as HTMLInputElement).value;
-    console.log(currentText);
-  }
+  // handleInputKeyup(event: KeyboardEvent)
+  // {
+  //   const currentText = (event.target as HTMLInputElement).value;
+  //   console.log(currentText);
+  // }
 
-  handleInputEvent(event: Event)
-  {
-    const currentText = (event.target as HTMLInputElement).value;
-    console.log(currentText);
-  }
+  // handleInputEvent(event: Event)
+  // {
+  //   const currentText = (event.target as HTMLInputElement).value;
+  //   console.log(currentText);
+  // }
 
-  buttonTitle = "Mas qui butão de trem sô";
-  buttonDisabled = true;
+  // buttonTitle = "Mas qui butão de trem sô";
+  // buttonDisabled = true;
 
-  onButtonClick()
-  {
-    this.buttonTitle = "Butão alteradu";
-    this.buttonDisabled = !this.buttonDisabled;
-  }
+  // onButtonClick()
+  // {
+  //   this.buttonTitle = "Butão alteradu";
+  //   this.buttonDisabled = !this.buttonDisabled;
+  // }
+
+  // widthButton1 = '110px';
+  // widthButton2 = 130;
+  // styleObj =
+  // {
+  //   width: '160px',
+  //   backgroundColor: 'grey',
+  // };
+
+  // updateStyleObj()
+  // {
+  //   console.log('updateStyleObj');
+
+  //   this.styleObj.width = '170px';
+  //   this.styleObj.backgroundColor = 'lightblue';
+  // }
+
+  // updateStyleObjCorrect()
+  // {
+  //   console.log('updateStyleObjCorrect');
+
+  //   this.styleObj = {
+  //     width: '180px',
+  //     backgroundColor: 'lightgreen',
+  //   };
+  // };
+
+  // updateWidth()
+  // {
+  //   this.widthButton2 += 10;
+  // }
+
+  isRedButton = true;
+  isGreen = true;
 }
