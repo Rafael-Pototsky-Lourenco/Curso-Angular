7032f8c7e37c07f505f4ca429d3a758e9a884395
Rafael P Lourenço <rafael.lourenco@consilux.com.br>
2025-01-23 14:50:55 -0300

    implement ng-if

--- Seção 4 - Fundamentos do Angular - Binding Diretivas, Templates e Decorators/Aula 66 - Ng If/src/app/app.component.ts
+++ Seção 4 - Fundamentos do Angular - Binding Diretivas, Templates e Decorators/Aula 66 - Ng If/src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'NG_IF';
+  minhaProp = true;
+  pessoa: any =
+  {
+    status: 'Hihihaw',
+    name: undefined
+  }
+
+  toggleProp()
+  {
+    this.minhaProp = !this.minhaProp;
+  }
+
+  isTruthy()
+  {
+    // return {};
+    // return [];
+    // return true;
+    // return 1;
+    // return "teste";
+    // return 'teste';
+    // return "0";
+    // return "false";
+    // return this.pessoa.status;
+  }
+
+  isFalsy()
+  {
+    // return false;
+    // return 0;
+    // return "";
+    // return '';
+    // return null;
+    // return undefined;
+    // return -0;
+    // return NaN;
+    // return this.pessoa.name;
+    // return this.pessoa.endereco?.rua
+  }
 }
