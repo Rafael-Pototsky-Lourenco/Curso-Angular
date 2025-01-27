61efadc90dc1d800483f3486c4b5c3bcc7461c9f
Rafael P Lourenço <rafael.lourenco@consilux.com.br>
2025-01-27 15:28:46 -0300

    finished ng style

--- Seção 4 - Fundamentos do Angular - Binding Diretivas, Templates e Decorators/Aula 69 (Funny Number) - Ng Style/src/app/app.component.ts
+++ Seção 4 - Fundamentos do Angular - Binding Diretivas, Templates e Decorators/Aula 69 (Funny Number) - Ng Style/src/app/app.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'NG_STYLE';
+  fontSize: number = 150;
+  textColor: 'white' | 'orange' = 'white'; 
+  buttonText: 'Branco' | 'Laranja' = 'Laranja';
+  stylesString: string = 'font-size: 150px; color: white;';
+  stylesObj: any = {
+    'font-size': this.fontSize + 'px',
+    'color': this.textColor,
+  }
+
+  increaseFontSize()
+  {
+    this.fontSize += 10;
+
+    this.stylesString = `font-size: ${this.fontSize}px; color: ${this.textColor};`;
+
+    this.stylesObj = {
+      'font-size': this.fontSize + 'px',
+      'color': this.textColor,
+    }
+  }
+
+  toggleFontColor()
+  {
+    if(this.textColor === 'white')
+    {
+      this.textColor = 'orange';
+      this.buttonText = 'Branco';
+    }
+    else
+    {
+      this.textColor = 'white';
+      this.buttonText = 'Laranja';
+    }
+
+    this.stylesString = `font-size: ${this.fontSize}px; color: ${this.textColor};`;
+
+    this.stylesObj = {
+      'font-size': this.fontSize + 'px',
+      'color': this.textColor,
+    }
+  }
 }
