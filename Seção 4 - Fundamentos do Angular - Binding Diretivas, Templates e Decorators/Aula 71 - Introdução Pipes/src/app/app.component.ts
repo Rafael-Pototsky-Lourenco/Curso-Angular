1944b35db23cb350a1cdf752c5740316a5f352a0
Rafael P Lourenço <rafael.lourenco@consilux.com.br>
2025-01-27 17:55:47 -0300

    add statusClass pipe to transform numeric status to string representation

--- Seção 4 - Fundamentos do Angular - Binding Diretivas, Templates e Decorators/Aula 71 - Introdução Pipes/src/app/app.component.ts
+++ Seção 4 - Fundamentos do Angular - Binding Diretivas, Templates e Decorators/Aula 71 - Introdução Pipes/src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'INTRO_PIPES';
+  text = 'Feef';
+  pessoa =
+  {
+    name: 'Geeg',
+    status: 1,
+  };
+
+  pessoa2 =
+  {
+    name: 'Jooj',
+    status: 2,
+  };
+
+  pessoa3 =
+  {
+    name: 'Kook',
+    status: 3,
+  };
+
+  getStyle(status: number)
+  {
+    console.log('getStyle');
+
+    return {
+      'active': status === 1,
+      'partial': status === 2,
+      'blocked': status === 3,
+    }
+  }
 }
