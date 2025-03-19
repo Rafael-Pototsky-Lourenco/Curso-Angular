0b9a1e0d34a9bfbf25cc01a88650d5471abac9c1
Rafael P Lourenço <rafael.lourenco@consilux.com.br>
2025-03-19 12:05:02 -0300

    Aula 112 Finalizada.

--- Seção 6 - Fundamentos do Angular - Diretivas, Templates e Decorators Dinâmicos/Aula 112 á 117 - Elemento Ref/src/app/app.component.ts
+++ Seção 6 - Fundamentos do Angular - Diretivas, Templates e Decorators Dinâmicos/Aula 112 á 117 - Elemento Ref/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'projeto-element-ref';
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('minhaDiv') divEl!: ElementRef<HTMLDivElement>;
+
+  constructor(private readonly _elRef: ElementRef) { }
+
+  ngOnInit() {
+    console.log(this._elRef);
+
+    const elDiv = this._elRef.nativeElement.querySelector('#outra-div') as HTMLDivElement;
+
+    elDiv.textContent = 'Sou um outra DIO!';
+    elDiv.style.backgroundColor = 'blue';
+    elDiv.style.color = 'fuchsia';
+
+    elDiv.addEventListener('click', () => {
+      console.log('Clicou na DIO!');
+    })
+
+    console.log(elDiv);
+  }
+
+  ngAfterViewInit() {
+    this.divEl.nativeElement.style.backgroundColor = 'orange';
+    this.divEl.nativeElement.textContent = 'EU SOU UMA DIO!';
+    this.divEl.nativeElement.classList.add('minha-classe');
+  }
 }
