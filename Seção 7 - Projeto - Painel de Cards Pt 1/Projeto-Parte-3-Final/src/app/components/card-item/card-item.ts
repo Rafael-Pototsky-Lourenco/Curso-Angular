56118d1e7e1e095272e362b9266582f970076e33
Rafael P Lourenço <rafael.lourenco@consilux.com.br>
2025-11-06 17:54:34 -0300

    Módulo 7, Aula 139: Começado a parte 3 do projeto em angular (Dessa vez seguindo passo a passo do professor).

---
+++ Seção 7 - Projeto - Painel de Cards Pt 1/Projeto-Parte-3-Final/src/app/components/card-item/card-item.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-item',
+  imports: [],
+  templateUrl: './card-item.html',
+  styleUrl: './card-item.scss',
+})
+export class CardItem {
+
+}
