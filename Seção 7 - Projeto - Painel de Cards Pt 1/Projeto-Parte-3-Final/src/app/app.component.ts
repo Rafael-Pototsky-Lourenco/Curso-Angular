505996d0d4f383ad06084c74d78735cb450dd559
Rafael P Lourenço <rafael.lourenco@consilux.com.br>
2025-11-11 16:32:42 -0300

    Módulo 7, Aula 139: Continuado a mudança do projeto para componentes.

--- Seção 7 - Projeto - Painel de Cards Pt 1/Projeto-Parte-3-Final/src/app/app.component.ts
+++ Seção 7 - Projeto - Painel de Cards Pt 1/Projeto-Parte-3-Final/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CardItem } from "./components/card-item/card-item";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [CardItem]
 })
 export class AppComponent {
   title = 'Projeto-Parte-3-Final';
