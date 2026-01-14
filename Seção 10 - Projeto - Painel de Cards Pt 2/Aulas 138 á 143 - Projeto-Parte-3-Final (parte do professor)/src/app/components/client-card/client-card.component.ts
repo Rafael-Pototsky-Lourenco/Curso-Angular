658cfd563ffb1bcf35b04d69f6dfc45c88e72440
Rafael P Lourenço <rafael.lourenco@consilux.com.br>
2026-01-14 17:44:49 -0300

    Módulo 10, Aula 162: Feito uma segunda aplicação da diretiva de cor, sendo dessa vez com troca de cor.

--- Seção 10 - Projeto - Painel de Cards Pt 2/Aulas 138 á 143 - Projeto-Parte-3-Final (parte do professor)/src/app/components/client-card/client-card.component.ts
+++ Seção 10 - Projeto - Painel de Cards Pt 2/Aulas 138 á 143 - Projeto-Parte-3-Final (parte do professor)/src/app/components/client-card/client-card.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-client-card',
   templateUrl: './client-card.component.html',
   styleUrls: ['./client-card.component.scss']
 })
-export class ClientCardComponent {
+export class ClientCardComponent implements OnInit{
+  headerColor: string = 'cyan';
 
+  ngOnInit() {
+    setTimeout(() => {
+      this.headerColor = 'wine';
+    }, 3000);
+  }
 }
